7028b4bff623e2fad9941d3a93b47b3ef36de967
minhluc073 <nguyenminhluc0703@gmail.com>
2023-08-15 17:12:03 +0700

    update

--- src/components/faq/index.jsx
+++ src/components/faq/index.jsx
@@ -30,62 +30,18 @@ function FaqItem({ data }) {
 
               <h6>Suggestion</h6>
               <div className="flat-accordion">
-                <div className="flat-toggle">
-                  <div className="toggle-title">
-                    When is the last time you can remember feeling ?{"{"}" "
-                    {"}"}
-                    <span className="icon" />
-                  </div>
-                  <div className="toggle-content">
-                    <p>
-                      Aenean porta, eros lacus congue lorem, samet mollis magna
-                      velit ac erat. Donec ut interdum arcu, egestas liber.
-                      Etiam condimentum aliquam accumsan. Morbi accumsan purus
-                      nec vehicula ornare.
-                    </p>
-                  </div>
-                </div>
-                <div className="flat-toggle">
-                  <div className="toggle-title">
-                    Which of the Seven Wonders of the world do you?{"{"}" "{"}"}
-                    <span className="icon" />
-                  </div>
-                  <div className="toggle-content">
-                    <p>
-                      Aenean porta, eros lacus congue lorem, samet mollis magna
-                      velit ac erat. Donec ut interdum arcu, egestas liber.
-                      Etiam condimentum aliquam accumsan. Morbi accumsan purus
-                      nec vehicula ornare.
-                    </p>
-                  </div>
-                </div>
-                <div className="flat-toggle">
-                  <div className="toggle-title">
-                    If you could change careers right this second{"{"}" "{"}"}
-                    <span className="icon" />
-                  </div>
-                  <div className="toggle-content">
-                    <p>
-                      Aenean porta, eros lacus congue lorem, samet mollis magna
-                      velit ac erat. Donec ut interdum arcu, egestas liber.
-                      Etiam condimentum aliquam accumsan. Morbi accumsan purus
-                      nec vehicula ornare.
-                    </p>
-                  </div>
-                </div>
-                <div className="flat-toggle">
-                  <div className="toggle-title">
-                    What makes you happiest? <span className="icon" />
-                  </div>
-                  <div className="toggle-content">
-                    <p>
-                      Aenean porta, eros lacus congue lorem, samet mollis magna
-                      velit ac erat. Donec ut interdum arcu, egestas liber.
-                      Etiam condimentum aliquam accumsan. Morbi accumsan purus
-                      nec vehicula ornare.
-                    </p>
-                  </div>
-                </div>
+                {data.slice(4, 8).map((item) => (
+                  <Accordion
+                    key={item.id}
+                    title={item.title}
+                    show={item.show}
+                    className="flat-toggle"
+                  >
+                    <div className="toggle-content">
+                      <p>{item.content}</p>
+                    </div>
+                  </Accordion>
+                ))}
               </div>
             </div>
           </div>
